3156c06283d1c38b2997080a069326f6e0c233de
Patrick Mackle <pimackle@ucdavis.edu>
2022-07-08 09:52:44 -0700

    cleaned some comments

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,10 @@ import "./App.css";
 import Login from "./pages/Login";
 
 function App() {
-  // const [backendData, setBackendData] = useState([{}]);
-  // useEffect(() => {
-  //   fetch('/api')
-  //     .then((res) => res.json())
-  //     .then((data) => setBackendData(data));
-  // }, []);
-
   return (
     <div>
       <h1>Welcome to MyTelecom</h1>
       <Login />
-      {/* {typeof backendData === 'undefined' ? (
-        <p>Loading...</p>
-      ) : (
-        backendData.users.map((user, i) => <p key={i}>{user}</p>)
-      )} */}
     </div>
   );
 }
