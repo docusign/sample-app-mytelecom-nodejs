f93c619f4ed3573753086ec2f3ade8eef0e45b5b
andrey.kandaurov <andrey.kandaurov@sigma.software>
2022-12-12 19:18:48 +0200

    Assumption of liability

--- client/src/pages/assumptionLiability/components/AssumptionLiabilityForm.js
+++ client/src/pages/assumptionLiability/components/AssumptionLiabilityForm.js
@@ -1,14 +1,11 @@
-import React from "react";
-import text from "../../../assets/Text.json";
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message";
-import {
-  emailRegExp,
-  formCheckNameMaxLength,
-  formCheckFieldRequired,
-} from "../../../components/CommonFormObjects";
+import React from 'react';
+import text from '../../../assets/Text.json';
+import { useForm } from 'react-hook-form';
+import { emailRegExp, formCheckNameMaxLength, formCheckFieldRequired } from '../../../components/CommonFormObjects';
+import { Button, Row, Col, Form } from 'react-bootstrap';
+import Input from './Input';
 
-function AssumptionLiabilityForm({ onSubmit }) {
+const AssumptionLiabilityForm = ({ onSubmit }) => {
   // Grab register and handleSubmit from useForm hook
   const {
     register,
@@ -16,142 +13,114 @@ function AssumptionLiabilityForm({ onSubmit }) {
     formState: { errors },
   } = useForm();
 
-  const ErrorMessageContainer = ({ children }) => (
-    <span className="error">{children}</span>
-  );
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <>
-        <label>{text.formLabels.firstName}</label>
-        <input
-          type="text"
-          {...register("firstName", {
-            required: {
-              value: true,
-              message: text.formLabels.requiredFieldError,
-            },
-            maxLength: {
-              value: 30,
-              message: text.formLabels.inputTooLongError,
-            },
-          })}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="firstName"
-          as={<ErrorMessageContainer />}
-        />
-        <p></p>
-        <label>{text.formLabels.lastName}</label>
-        <input
-          type="text"
-          {...register("lastName", {
-            required: formCheckFieldRequired,
-            maxLength: formCheckNameMaxLength,
-          })}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="lastName"
-          as={<ErrorMessageContainer />}
-        />
-        <p></p>
-        <label>{text.formLabels.email}</label>
-        <input
-          type="text"
-          {...register("signerEmail", {
-            required: formCheckFieldRequired,
-            pattern: {
-              value: emailRegExp,
-              message: text.formLabels.invalidEmailFormatError,
-            },
-          })}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="signerEmail"
-          as={<ErrorMessageContainer />}
-        />
-        <p></p>
-        <label>{text.formLabels.newPhoneOwnerInfo}</label>
-        <p></p>
-        <label>{text.formLabels.firstName}</label>
-        <input
-          type="text"
-          {...register("recipientFirstName", {
-            required: formCheckFieldRequired,
-            maxLength: formCheckNameMaxLength,
-          })}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="firstName"
-          as={<ErrorMessageContainer />}
-        />
-        <p></p>
-        <label>{text.formLabels.lastName}</label>
-        <input
-          type="text"
-          {...register("recipientLastName", {
-            required: formCheckFieldRequired,
-            maxLength: formCheckNameMaxLength,
-          })}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="lastName"
-          as={<ErrorMessageContainer />}
-        />
-        <p></p>
-        <label>{text.formLabels.countryCode}</label>
-        <input
-          type="text"
-          placeholder="1"
-          {...register("countryCode", {
-            required: formCheckFieldRequired,
-            pattern: {
-              value: /^([+]?\d+)$/,
-              message: text.formLabels.invalidCountryCodeError,
-            },
-          })}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="countryCode"
-          as={<ErrorMessageContainer />}
-        />
-        <p></p>
-        <label>{text.formLabels.phoneNumber}</label>
-        <input
-          type="text"
-          {...register("phoneNumber", {
-            required: formCheckFieldRequired,
-            pattern: {
-              value: /^(\d+-?)+\d+$/,
-              message: text.formLabels.invalidPhoneNumberError,
-            },
-            maxLength: {
-              value: 11,
-              message: text.formLabels.invalidPhoneNumberError,
-            },
-            minLength: {
-              value: 8,
-              message: text.formLabels.invalidPhoneNumberError,
-            },
-          })}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="phoneNumber"
-          as={<ErrorMessageContainer />}
-        />
-      </>
-      <p></p>
-
-      <input type="submit" value={text.formLabels.buttonName} />
-    </form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <h6>{text.formLabels.currentAccountOwner}</h6>
+      <Input
+        id="firstName"
+        label={text.formLabels.firstName}
+        labelProps={{ column: true, sm: 4 }}
+        validations={{
+          required: formCheckFieldRequired,
+          maxLength: formCheckNameMaxLength,
+        }}
+        register={register}
+        errors={errors}
+      />
+      <Input
+        id="lastName"
+        label={text.formLabels.lastName}
+        labelProps={{ column: true, sm: 4 }}
+        validations={{
+          required: formCheckFieldRequired,
+          maxLength: formCheckNameMaxLength,
+        }}
+        register={register}
+        errors={errors}
+      />
+      <Input
+        id="signerEmail"
+        label={text.formLabels.email}
+        labelProps={{ column: true, sm: 4 }}
+        validations={{
+          required: formCheckFieldRequired,
+          pattern: {
+            value: emailRegExp,
+            message: text.formLabels.invalidEmailFormatError,
+          },
+        }}
+        register={register}
+        errors={errors}
+      />
+      <hr />
+      <h6>{text.formLabels.newAccountOwner}</h6>
+      <Input
+        id="recipientFirstName"
+        label={text.formLabels.firstName}
+        labelProps={{ column: true, sm: 4 }}
+        validations={{
+          required: formCheckFieldRequired,
+          maxLength: formCheckNameMaxLength,
+        }}
+        register={register}
+        errors={errors}
+      />
+      <Input
+        id="recipientLastName"
+        label={text.formLabels.lastName}
+        labelProps={{ column: true, sm: 4 }}
+        validations={{
+          required: formCheckFieldRequired,
+          maxLength: formCheckNameMaxLength,
+        }}
+        register={register}
+        errors={errors}
+      />
+      <Row>
+        <Col sm={4}>
+          <Input
+            id="countryCode"
+            label={text.formLabels.countryCode}
+            validations={{
+              required: formCheckFieldRequired,
+              pattern: {
+                value: /^([+]?\d+)$/,
+                message: text.formLabels.invalidCountryCodeError,
+              },
+            }}
+            register={register}
+            errors={errors}
+          />
+        </Col>
+        <Col>
+          <Input
+            id="phoneNumber"
+            label={text.formLabels.phoneNumber}
+            validations={{
+              required: formCheckFieldRequired,
+              pattern: {
+                value: /^(\d+-?)+\d+$/,
+                message: text.formLabels.invalidPhoneNumberError,
+              },
+              maxLength: {
+                value: 11,
+                message: text.formLabels.invalidPhoneNumberError,
+              },
+              minLength: {
+                value: 8,
+                message: text.formLabels.invalidPhoneNumberError,
+              },
+            }}
+            register={register}
+            errors={errors}
+          />
+        </Col>
+      </Row>
+      <Button type="submit" className="h-card-button">
+        {text.formLabels.buttonName}
+      </Button>
+    </Form>
   );
-}
+};
 
 export default AssumptionLiabilityForm;
--- client/src/pages/assumptionLiability/index.js
+++ client/src/pages/assumptionLiability/index.js
@@ -1,23 +1,24 @@
-import React from "react";
-import { sendRequest } from "../../api/apiHelper";
-import AssumptionLiabilityForm from "./components/AssumptionLiabilityForm";
-import text from "../../assets/Text.json";
+import React from 'react';
+import { sendRequest } from '../../api/apiHelper';
+import AssumptionLiabilityForm from './components/AssumptionLiabilityForm';
+import text from '../../assets/Text.json';
+import { Col, Container, Row } from 'react-bootstrap';
 
-function Liability() {
-  async function handleSubmit(event) {
+const Liability = () => {
+  const handleSubmit = async (event) => {
     // Make request body
     const body = {
-      signerName: event.firstName + " " + event.lastName,
+      signerName: event.firstName + ' ' + event.lastName,
       signerEmail: event.signerEmail,
-      recipientName: event.recipientFirstName + " " + event.recipientLastName,
+      recipientName: event.recipientFirstName + ' ' + event.recipientLastName,
       recipientPhone: event.phoneNumber,
       recipientCountryCode: event.countryCode,
     };
 
     // Send request to server
     try {
-      const response = await sendRequest("/assumptionLiability", body);
-      console.log("Received response:");
+      const response = await sendRequest('/assumptionLiability', body);
+      console.log('Received response:');
       console.log(response.data);
 
       // Received URL for embedded signing, redirect user
@@ -25,17 +26,24 @@ function Liability() {
         window.location = response.data;
       }
     } catch (error) {
-      console.log("handleSubmit error");
+      console.log('handleSubmit error');
       console.log(error);
     }
-  }
+  };
 
   return (
-    <div>
-      <h1>{text.titles.assumptionTitle}</h1>
-      <AssumptionLiabilityForm onSubmit={handleSubmit} />
-    </div>
+    <section className="content-section">
+      <Container>
+        <Row className="justify-content-center">
+          <Col className="form-holder">
+            <h4>{text.titles.assumptionTitle}</h4>
+            <AssumptionLiabilityForm onSubmit={handleSubmit} />
+          </Col>
+          <Col className="bs-holder" />
+        </Row>
+      </Container>
+    </section>
   );
-}
+};
 
 export default Liability;
