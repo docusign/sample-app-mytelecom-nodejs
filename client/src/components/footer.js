8492019bbdda18dcc53d255f133e8e9513f3497a
inbargazit <inbar.gazit@docusign.com>
2025-01-27 14:42:07 -0800

    Removing marking lingo

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,9 +5,6 @@ const Footer = () => {
   const { t } = useTranslation('Common');
   return (
     <footer className="footer">
-      <div className="container text-center pt-3">
-        <span className="copyright">{t('Copyright')}</span>
-      </div>
     </footer>
   );
 };
