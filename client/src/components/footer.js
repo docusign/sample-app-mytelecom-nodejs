4c54a55fb4601172dd895209d25e5950a44d0c2c
inbargazit <inbar.gazit@docusign.com>
2025-01-27 14:57:25 -0800

    More work on the rename

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,8 +5,7 @@ const Footer = () => {
   const { t } = useTranslation('Common');
   return (
     <footer className="footer">
-      <div className="container text-center pt-3">
-      </div>
+      <div className="container text-center pt-3"></div>
     </footer>
   );
 };
